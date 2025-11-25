de32b3e80043e8da40333c3e879a3a306aab8d5d
faeze-yousefi <faezehyousefiii18@gmail.com>
2025-11-26 01:23:35 +0330

    back button for supplier https://trello.com/c/APZJRPRQ

--- supplier.py
+++ supplier.py
@@ -1,10 +1,14 @@
-from tkinter import *
+from tkinter import * 
 
 def supplier_form(window):
-    print("Supplier button clicked!")   # ← این خط را اضافه کن فقط برای تست
-    win = Toplevel(window)
-    win.title("تامین‌کنندگان")
-    win.geometry("800x500")
-    win.resizable(False, False)
+     global back_image
+     supplier_frame = Frame(window, width=1165, height=567, bg='white') 
+     supplier_frame.place(x=200, y=100)
+     heading_label = Label(supplier_frame, text='مدیریت تامین کنندگان', font=('fonts/Persian-Yekan.ttf', 16, 'bold'),
+                          bg='#00198f', fg='white')
+     heading_label.place(x=0, y=0, relwidth=1)
+     back_image = PhotoImage(file='images/back_button.png')
 
-    Label(win, text="صفحه تامین‌کنندگان", font=('Arial', 20)).pack(pady=20)
+     back_button = Button(supplier_frame, image=back_image, bd=0, cursor='hand2', bg='white',
+                         command=lambda: supplier_frame.place_forget())
+     back_button.place(x=10, y=30)
