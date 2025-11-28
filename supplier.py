50632eb7abc2334bbe207d19ae10b962912888d5
faeze-yousefi <faezehyousefiii18@gmail.com>
2025-11-28 19:55:57 +0330

    Finish the right frame https://trello.com/c/iOSKtIkB

--- supplier.py
+++ supplier.py
@@ -34,7 +34,7 @@ def supplier_form(window):
      global back_image
      supplier_frame = Frame(window, width=1165, height=567, bg='white')
      supplier_frame.place(x=200, y=100)
-     heading_label = Label(supplier_frame, text='مدیریت تامین کنندگان', font=('fonts/Persian-Yekan.ttf', 16, 'bold'),
+     heading_label = Label(supplier_frame, text='مدیریت تامین کنندگان', font=('fonts/Persian-Yekan.ttf', 18, 'bold'),
                           bg='#00198f', fg='white')
      heading_label.place(x=0, y=0, relwidth=1)
      back_image = PhotoImage(file='images/back_button.png')
